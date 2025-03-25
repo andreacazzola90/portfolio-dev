999d693ea2170fe3dbca1ff5475afcd00ca43f73
andreacazzola90 <andreacazzola90@gmail.com>
2025-03-25 15:10:21 +0100

    start project

---
+++ data/data.ts
@@ -0,0 +1,12 @@
+export const links = {
+  home: "/",
+  about: "/about",
+  work: "/work",
+  contact: "/contact",
+  email: "mailto:andreacazzola90@gmail.com",
+  resume: "/resume",
+  github: "https://github.com/andreacazzola90",
+  linkedin: "https://www.linkedin.com/in/andreacazzola/",
+  mastodon: "https://mastodon.social/@andreacazzola90",
+  codepen: "https://codepen.io/andreacazzola90",
+};
