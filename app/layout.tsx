39df2a1055eb4e49ac0f3456e957e560671643b5
Andrea <andreacazzola90@gmail.com>
2025-08-29 11:42:25 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import StoreProvider from "@/redux/storeProvider";
 import { gsap } from "gsap";
 import { CustomEase } from "gsap/CustomEase";
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/next';
 gsap.registerPlugin(CustomEase);
 
 const dM_Sans = DM_Sans({ subsets: ["latin-ext"] });
@@ -52,10 +53,12 @@ export default function RootLayout({
       <body className={helvetica.className}>
         <StoreProvider>
           {children}
+          <GoogleTagManager gtmId="GTM-TL5J9KWX" />
+          <GoogleAnalytics gaId="G-P42JKPYWYQ" />
+          <Analytics />
         </StoreProvider>
-        <GoogleTagManager gtmId="GTM-TL5J9KWX" />
-        <GoogleAnalytics gaId="G-P42JKPYWYQ" />
       </body>
+      
       <Script src="https://cdn.jsdelivr.net/gh/vipulkumar-dev/gsap@2024/ScrambleTextPlugin.min.js" />
     </html>
   );
